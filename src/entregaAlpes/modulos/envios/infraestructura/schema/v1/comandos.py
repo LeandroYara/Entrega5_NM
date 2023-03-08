1b5630069738a175f261af32fb53d33caca6a6d2
Herald Matias Olivares Zarsoza <heraldmatias.oz@gmail.com>
2023-03-08 04:30:17 +0000

    Comandos de envio con schema

--- src/entregaAlpes/modulos/envios/infraestructura/schema/v1/comandos.py
+++ src/entregaAlpes/modulos/envios/infraestructura/schema/v1/comandos.py
@@ -2,9 +2,10 @@ from pulsar.schema import *
 from dataclasses import dataclass, field
 from entregaAlpes.seedwork.infraestructura.schema.v1.comandos import (ComandoIntegracion)
 
-class ComandoCrearReservaPayload(ComandoIntegracion):
+class ComandoEnviarPedidoPayload(ComandoIntegracion):
     id_usuario = String()
+    courier_name = String()
     # TODO Cree los records para itinerarios
 
 class ComandoCrearReserva(ComandoIntegracion):
-    data = ComandoCrearReservaPayload()
+    data = ComandoEnviarPedidoPayload()
